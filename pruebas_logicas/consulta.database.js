770616ed15aace2dd24cc449c7f5f1280a9808e1
carlos <carlos16qf@gmail.com>
2022-04-07 16:55:00 -0500

    prueba-tecnica2

--- pruebas_logicas/consulta.database.js
+++ pruebas_logicas/consulta.database.js
@@ -8,7 +8,7 @@
 //select count(*) as numero_usuarios from usuario where direccion -> 'cp' = '39005';
 
 //cuarta consulta
-//
+// select * from usuario where telefonos = '111111111'
 
 //quinta consulta
-//
+//select nombre_usuario, cuenta_twitter count(*) as numero_usuarios from usuario where direccion-> 'cp' = '39005
